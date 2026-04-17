3aff51033f0d8fbe104bb81a968f41b87f6ea350
Steve Ross <git.commits@mailbox.org>
2026-04-17 18:35:56 +0100

    block: core/embed -- styling

--- web/app/themes/badegg/resources/views/blocks/core/Embed.jsx
+++ web/app/themes/badegg/resources/views/blocks/core/Embed.jsx
@@ -16,17 +16,15 @@ export default function Embed({ index, name, attributes }) {
       `wp-block-embed-${ index }`,
       provider && `wp-block-embed-${ provider } is-provider-${ provider }`,
       className && className,
-      'wp-embed-aspect-16-9',
-      'wp-has-aspect-ratio',
       type && `is-type-${ type }`,
     ),
   }
 
   const EmbedSelector = ( props ) => {
     switch (provider) {
-      case 'youtube' :  return <Youtube     { ...props } />
-      // case 'vimeo'   :  return <Vimeo       { ...props } />
-      default        :  return <Unsupported { ...props } />
+      case 'youtube' :  return  <Youtube     { ...props } />
+      case 'vimeo'   :  return  <Vimeo       { ...props } />
+      default        :  return  <Unsupported { ...props } />
     }
   }
 
