e1a9c6d6f3fe290e857d3df7717591ef7a55a5c1
Steve Ross <git.commits@mailbox.org>
2026-04-30 18:00:34 +0100

    block: core MediaText

--- web/app/themes/badegg/resources/views/blocks/core/MediaText.jsx
+++ web/app/themes/badegg/resources/views/blocks/core/MediaText.jsx
@@ -1,9 +1,32 @@
 import './MediaText.scss'
+import parse, { attributesToProps } from "html-react-parser"
+import Switchboard from '@views/components/Switchboard/Switchboard'
 
-export default function MediaText(attributes) {
-  const { name, innerBlocks } = attributes;
+export default function MediaText( props ) {
+  const { rawContent, innerBlocks, attributes } = props;
+  const { mediaWidth } = attributes;
 
-  return (
-    <h3>{ name }</h3>
+  const TextCol = () => (
+    <>
+      { innerBlocks.map((block, index) => <Switchboard key={index} index={index} {...block} /> )}
+    </>
   )
+
+  const options = {
+    replace: (domNode) => {
+      if (
+        domNode.type === "tag" &&
+        domNode.name === "div" &&
+        domNode.attribs?.class?.includes("wp-block-media-text__content")
+      ) {
+        return (
+          <div className={ domNode.attribs.class }>
+            <TextCol />
+          </div>
+        )
+      }
+    },
+  }
+
+  return parse( rawContent, options );
 }
