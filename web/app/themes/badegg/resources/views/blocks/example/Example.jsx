4ff64b42fdf069ecba5d8353fc1a81b41a6f0efe
Steve Ross <git.commits@mailbox.org>
2026-05-09 21:29:29 +0100

    base markup for example block

--- web/app/themes/badegg/resources/views/blocks/example/Example.jsx
+++ web/app/themes/badegg/resources/views/blocks/example/Example.jsx
@@ -3,6 +3,10 @@ import './style.scss'
 export default function Example(attributes) {
 
   return (
-    <h2>Example Block</h2>
+    <section className="section wp-block-example bg-success">
+      <div className="container container-narrow align-centre wysiwyg">
+        <h2 className="white">Example Block</h2>
+      </div>
+    </section>
   )
 }
