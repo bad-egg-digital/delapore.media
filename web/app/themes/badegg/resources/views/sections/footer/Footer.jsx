454bc0466b5257067425c7fdae7f47cd4e705b3a
Steve Ross <git.commits@mailbox.org>
2026-05-09 15:18:02 +0100

    footer

--- web/app/themes/badegg/resources/views/sections/footer/Footer.jsx
+++ web/app/themes/badegg/resources/views/sections/footer/Footer.jsx
@@ -7,12 +7,17 @@ import Socials from '@views/components/Socials/Socials';
 
 export default function Footer() {
   const { appContext, setAppContext } = useContext( AppContext )
+  const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="content-info bg-black section">
+    <footer className="content-info section">
       <div className="container container-small align-centre">
         <Brand name={ appContext.companyName } tagline={ true } />
         <Socials />
+        <p className="copyright">
+          <span>&copy;{ ( currentYear == 2026 ) ? currentYear : '2026-' + currentYear } { appContext.company.nameLegal },</span>
+          <span>All Rights Reserved</span>
+        </p>
       </div>
     </footer>
   )
