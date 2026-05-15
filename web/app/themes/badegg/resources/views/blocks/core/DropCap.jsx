23fab8160d93c67c8c5e26d9b91c15fd2ebedd38
Steve Ross <git.commits@mailbox.org>
2026-05-15 16:12:59 +0100

    dropcap gradient and excerpt nested paragraph fix

--- web/app/themes/badegg/resources/views/blocks/core/DropCap.jsx
+++ web/app/themes/badegg/resources/views/blocks/core/DropCap.jsx
@@ -1,11 +1,24 @@
 import './DropCap.scss'
 import parse from "html-react-parser"
+import parseHtml from "@scripts/lib/parser"
+import { useEffect, useState } from 'react'
 
 export default function DropCap( props ) {
-  const { rawContent, textOverride } = props
+  const { content, rawContent, textOverride } = props
+  const [ text, setText ] = useState('')
+
+  useEffect(() => {
+    if(textOverride) {
+      const parsed = parseHtml(textOverride)
+
+      if(parsed.length > 0) {
+        setText( parsed[0].props.children )
+      }
+    }
+  }, [ textOverride ])
 
   if(textOverride) {
-    return <p className="has-drop-cap">{ parse(textOverride) }</p>
+    return <p className="has-drop-cap">{ text }</p>
   } else {
     return <>{ parse(rawContent) }</>
   }
