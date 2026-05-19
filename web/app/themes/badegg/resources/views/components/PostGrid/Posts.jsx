0287f992fa3223f1c7ed4e5c81359164729b7157
Steve Ross <git.commits@mailbox.org>
2026-05-19 13:26:42 +0100

    postGrid component display skeleton cards while loading

--- web/app/themes/badegg/resources/views/components/PostGrid/Posts.jsx
+++ web/app/themes/badegg/resources/views/components/PostGrid/Posts.jsx
@@ -1,6 +1,8 @@
+import clsx from 'clsx'
 import Card from '@views/components/Card/Card'
 
 export default function Posts( props ) {
+
   const {
     posts,
     postType = 'post',
@@ -8,41 +10,49 @@ export default function Posts( props ) {
     showLoading = false,
   } = props
 
+  const className = clsx(
+    'section',
+    'section-small',
+    'section-zero-bottom',
+    'postgrid',
+    postType && 'postgrid-' + postType,
+  )
+
   return (
-    <div className={ `postgrid postgrid-${ postType } section section-small section-zero-bottom` }>
-      { isLoaded && (
-        <>
-          { posts && posts.length > 0 && (
-            <>
+    <div className={ className }>
+      <div className="postgrid-items postgrid-fade">
+        { posts && posts.length > 0 ? (
+          <>
 
-              { posts.map( ( post, index ) =>
-                <Card
-                  key={ `${ postType }-${ index }` }
-                  postType={ postType }
-                  isLoaded={ isLoaded }
-                  { ...post.node }
-                />
-              )}
+            { posts.map( ( post, index ) =>
+              <Card
+                key={ `${ postType }-${ index }` }
+                postType={ postType }
+                isLoaded={ isLoaded }
+                { ...post.node }
+              />
+            )}
 
-            </>
+          </>
 
-          )}
-        </>
-      )}
+        ) : (
+          <>
+            { showLoading &&
+              <>
+                { [...Array(3).keys()].map( x =>
+                  <Card
+                    key={ `loading-card-${ x }` }
+                    postType={ postType }
+                    isLoaded={ isLoaded }
+                    showLoading={ showLoading }
+                  />
+                )}
+              </>
+            }
+          </>
+        )}
 
-      { !isLoaded && showLoading && (
-        <>
-          { [...Array(3).keys()].map( x =>
-            <Card
-              key={ `loading-card-${ x }` }
-              postType={ postType }
-              isLoaded={ isLoaded }
-              showLoading={ showLoading }
-            />
-          )}
-        </>
-      )}
-
+      </div>
     </div>
   )
 }
