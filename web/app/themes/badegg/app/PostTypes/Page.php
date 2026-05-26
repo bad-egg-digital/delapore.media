3932cd7c0bee6549e40184c03b9ed37d18f7a4c9
Steve Ross <git.commits@mailbox.org>
2026-05-26 18:37:17 +0100

    disable heirarchy for pages

--- web/app/themes/badegg/app/PostTypes/Page.php
+++ web/app/themes/badegg/app/PostTypes/Page.php
@@ -7,5 +7,28 @@ class Page
     public function __construct()
     {
         add_post_type_support( 'page', 'excerpt' );
+        add_filter( 'register_page_post_type_args', [$this, 'args'], 10, 2 );
+        add_action( 'pre_get_posts', [$this, 'order'], 10, 2 );
+    }
+
+    public function args($args)
+    {
+        $args['hierarchical'] = false;
+
+        return $args;
+    }
+
+    public function order($query)
+    {
+        if ( !is_admin() || !$query->is_main_query() ) {
+            return;
+        }
+
+        $screen = get_current_screen();
+        // change 'book' with your real CPT name
+        if ( $screen->base === 'edit' && $screen->post_type === 'page' ) {
+            $query->set('orderby', 'menu_order');
+            $query->set('order', 'ASC');
+        }
     }
 }
